711fc69c24b3ca69e35cdb746431a0fdecb8c9c7
Aleksandr <ichmarchuk@gail.com>
2026-02-18 21:42:36 +0100

    add SeatSelector => full version

--- src/components/eventBooking/index.jsx
+++ src/components/eventBooking/index.jsx
@@ -4,9 +4,16 @@ import EventDetails from "../eventDetails";
 import SeatSelector from "../seatSelector";
 
 function EventBooking() {
+  const [selectedSeats, setSelectedSeats] = useState([]);
+
   return (
     <div className={styles.container}>
       <EventDetails />
+      <SeatSelector
+        selectedSeats={selectedSeats}
+        setSelectedSeats={setSelectedSeats}
+      />
+      <h4>Selected seats: {selectedSeats.join(", ") || "none"}</h4>
     </div>
   );
 }
